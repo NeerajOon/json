420cd5310100155bcf37caed9ce49be6a1cdb16d
neeraj <neerajoon121@gmail.com>
2022-02-06 19:12:53 +0530

    first commit json

---
+++ JSON1.py
@@ -0,0 +1,86 @@
+import json
+
+
+# x = '{"1":20,"3":40,"4":509}'
+# y = json.loads(x)
+# print(y)
+# print(type(y))
+
+############################################################################################
+
+# j = '{ "Name":"Neeraj","From":"Indore","Age":25}'
+# p_obj =json.loads(j)
+# print(p_obj)
+# print("Name:",p_obj["Name"])
+# print("From:",p_obj["From"])
+# print("Age:",p_obj["Age"])
+
+##########################################################################
+
+# p = {
+#     "name":"neeraj",
+#     "from":"Indore",
+#     "age":25
+# }
+# print(p)
+# j_obj = json.dumps(p)
+# print(j_obj)
+############################################################################
+
+# p_dict = {"Name":"Neeraj","From":"Indore","Age":25}
+# p_list = ["neeraj","aamla","khategaon"]
+# p_tuple = ("apple", "banana", "cherry")
+# p_str = ("Neeraj Oon")
+# p_int = (2022)
+# p_float = (26.1)
+# p_T = (True)
+# p_F = (False)
+# p_N = (None)
+
+# j_dict = json.dumps(p_dict)
+# j_list = json.dumps(p_list)
+# j_tuple = json.dumps(p_tuple)
+# j_str = json.dumps(p_str)
+# j_int = json.dumps(p_int)
+# j_float = json.dumps(p_float)
+# j_T = json.dumps(p_T)
+# j_F = json.dumps(p_F)
+# j_N = json.dumps(p_N)
+
+# print("json dict:",j_dict)
+# print("json list:",j_list)
+# print("json tuple:",j_tuple)
+# print("json str:",j_str)
+# print("json int:",j_int)
+# print("json float:",j_float)
+# print("json true:",j_T)
+# print("json false:",j_F)
+# print("json null:",j_N)
+
+
+#######################################################################
+
+# j_str = {'1': 50, '2': 27, '3': 35, '4': 45}
+# print(j_str)
+# print(json.dumps(j_str, indent=4))
+
+#######################################################################
+
+# j_dict = '{"Name":"Neeraj","From":"Indore","Age":25}'
+# j_list = '["neeraj","aamla","khategaon"]'
+# j_str = ' "Neeraj Oon" '
+# j_int = '2022'
+# j_float = '26.1'
+
+# p_dict = json.loads(j_dict)
+# p_list = json.loads(j_list)
+# p_str = json.loads(j_str)
+# p_int = json.loads(j_int)
+# p_float = json.loads(j_float)
+
+# print("python dict:",p_dict)
+# print("python list:",p_list)
+# print("python str:",p_str)
+# print("python int:",p_int)
+# print("pyhton float:",p_float)
+
